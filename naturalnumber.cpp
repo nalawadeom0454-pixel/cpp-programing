b7ab540e53b21a56a28cfa7331975414fdf1c408
nalawadeom0454-pixel <nalawadeom.0454@gmail.com>
2026-07-21 05:11:03 +0000

    to print the first  n natural number

---
+++ naturalnumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter the natural number to check\n";
+cin>>a;
+if(a>0)
+{
+cout<<a<<"is natural number";
+}
+else{
+cout<<a<<"is not natural number";
+}}
