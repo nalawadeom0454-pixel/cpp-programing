93e55545af16a6513f774c087634114fe7aa60cc
nalawadeom0454-pixel <nalawadeom.0454@gmail.com>
2026-07-21 05:16:43 +0000

    swap two number without using third variable

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a, b, c;
+cout<<"enter two numbers to swap\n";
+cin>>a>>b;
+c=a;
+a=b;
+b=c;
+cout<<"after swapping a="<<a<<" and b="<<b;
+}
