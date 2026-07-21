a41e624e86519c791b5aa702fc2f93086e2824d1
nalawadeom0454-pixel <nalawadeom.0454@gmail.com>
2026-07-21 04:23:28 +0000

    even code

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter any number:";
+cin>> a ;
+if(a%2==0)
+{
+    cout<<"no is even";
+
+}
+else{
+    cout<<"no is odd";
+}
+
+}
