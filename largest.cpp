413e12ea02c66f0d8095ddf9db60bbefacbebff5
nalawadeom0454-pixel <nalawadeom.0454@gmail.com>
2026-07-21 04:45:45 +0000

    find out largest of three number

---
+++ largest.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+
+cout<<"enter any three numbers:";
+
+cin>>a>>b>>c;
+
+if(a>=b&&a>=c)
+{
+cout<<"greatest number is "<<a;
+} 
+else if
+
+(b>=a&&b>=c)
+{
+cout<<"greatest number is "<<b;
+} else {
+    cout<<"greatest number is "<<c;
+}
+}
